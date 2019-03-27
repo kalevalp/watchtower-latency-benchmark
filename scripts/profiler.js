380282458e103491f825cd189e1a0b66acf5cb32
Kalev Alpernas <kalev.alp@gmail.com>
2019-03-27 17:01:05 +0200

    profiler benchmark

--- scripts/profiler.js
+++ scripts/profiler.js
@@ -6,10 +6,16 @@ async function main() {
     const lgs = await scraper.getAllLogGroups();
     const monitorLog = lgs.find(item => item.match(/watchtower-monitor/));
     const monitorLogElements = await scraper.getAllLogItemsForGroup(monitorLog);
+
+    //console.log(monitorLogElements);
+    
     const violationLog = monitorLogElements.filter(x => x.message.match(/Property .* was violated/));
 
-    const violationRE = /Property dummy was violated for property instance \{"dummy":"([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})"\}. Failure triggered by event produced by Lambda invocation ([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}).\n/
+    const violationRE = /Property dummy was violated for property instance \{"dummy":"([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})"\}. Failure triggered by event produced by Lambda invocation ([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}). Kinesis arrival timestamp @@([0-9]{10}\.[0-9]{0,3})@@.\n/
+    const monitorInvocationRE = /([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\tProperty dummy/
+    const invocationRE = /START RequestId: ([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}) Version: \$LATEST\n/;
 
+    
     const violationTimes = {}
     
     for (violation of violationLog) {
@@ -17,30 +23,81 @@ async function main() {
 	violationTimes[matched[1]] = {}
 	violationTimes[matched[1]].detected = violation.timestamp;
 	violationTimes[matched[1]].violatingInvocation = matched[2];
-	
-	// console.log(violationTimes);
+	violationTimes[matched[1]].kinesisIngectionTime = matched[3];
+
+	const monitorInvocationID = violation.message.match(monitorInvocationRE)[1];
+	const monitorInvocationLogElements = monitorLogElements.filter(x => x.message.match(invocationRE)).filter(x => x.message.match(monitorInvocationID));
+
+	if (monitorInvocationLogElements.length === 1) {
+	    violationTimes[matched[1]].monitorInvoked = monitorInvocationLogElements[0].timestamp;
+	} else if (monitorInvocationLogElements.length > 1) {
+	    throw `Expected only a single monitor invocation to match, got ${monitorInvocationLogElements.length}!`;
+	    
+	}
+
     }
     
     const ingestionLog = lgs.find(item => item.match(/watchtower-ingestion/));
     const ingestionLogElements = await scraper.getAllLogItemsForGroup(ingestionLog);
 
-    const ingestionInvocationRE = /START RequestId: ([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}) Version: \$LATEST\n/;
-
-    const ingestionFunctionInvocations = ingestionLogElements.filter(elem => elem.message.match(ingestionInvocationRE));
+    const ingestionFunctionInvocations = ingestionLogElements.filter(elem => elem.message.match(invocationRE));
     
     for (ingestionInvocation of ingestionFunctionInvocations) {
-	const ingestionInvocationUuid = ingestionInvocation.message.match(ingestionInvocationRE)[1];
+	const ingestionInvocationUuid = ingestionInvocation.message.match(invocationRE)[1];
 	const ingestionInvocationTime = ingestionInvocation.timestamp;
 
 	const ingestionInvocationElements = ingestionLogElements.filter(x => x.message.match(ingestionInvocationUuid));
 
 	for (violatingEventUuid in violationTimes) {
-	    console.log(ingestionInvocationElements.filter(x => x.message.match(violatingEventUuid)));
+	    const violatingEventIngestionLogItems = ingestionInvocationElements.filter(x => x.message.match(violatingEventUuid));
+
+	    if (violatingEventIngestionLogItems.length === 1) {
+		const violatingEventIngestionLogItem = violatingEventIngestionLogItems[0];	
+		violationTimes[violatingEventUuid].violationTime = violatingEventIngestionLogItem.message.match(/timestamp: ([0-9]{13})/)[1];
+		violationTimes[violatingEventUuid].ingestionInvocationTime = ingestionInvocationTime;
+	    } else if (violatingEventIngestionLogItems > 1) {
+		throw "Expected only a single violating event!"
+	    }
 	}
-	
-	
+    }
+
+    const lambdaLog = lgs.find(item => item.match(/hello/));
+    const lambdaLogElements = await scraper.getAllLogItemsForGroup(lambdaLog);
+
+    for (violatingEventUuid in violationTimes) {
+	const lambdaLogViolations = lambdaLogElements.filter(x => x.message.match(violatingEventUuid));
+	if (lambdaLogViolations.length === 1) {
+	    violationTimes[violatingEventUuid].originalViolationCloudWatchIngestionTime = lambdaLogViolations[0].ingestionTime
+	} else if (lambdaLogViolations > 1) {
+	    throw "Expected only a single violating event in original lambda log!"
+	}
+
+    }
+
+    for (violatingEventUuid in violationTimes) {
+	violationTimes[violatingEventUuid].kinesisIngectionTime = Number(violationTimes[violatingEventUuid].kinesisIngectionTime) * 1000;
+	violationTimes[violatingEventUuid].violationTime        = Number(violationTimes[violatingEventUuid].violationTime);
+    }
+
+    for (violatingEventUuid in violationTimes) {
+	const report = violationTimes[violatingEventUuid];
+	console.log(`
+Profiler report for violating event ${violatingEventUuid}:
+  Total detection delay: ${report.detected - report.violationTime}(ms).
+  Of which:
+    CloudWatch Log ingestion delay is: ${report.originalViolationCloudWatchIngestionTime - report.violationTime}(ms),
+    Log listener invocation delay is:  ${report.ingestionInvocationTime - report.originalViolationCloudWatchIngestionTime}(ms),
+    Kinesis ingestion delay is:        ${report.kinesisIngectionTime - report.ingestionInvocationTime}(ms),
+    Monitor invocation delay is:       ${report.monitorInvoked - report.kinesisIngectionTime}(ms),
+    Monitor runtime is:                ${report.detected - report.monitorInvoked}(ms)
+`);
     }
     
+    
+    
+    
+    console.log(violationTimes);
+
 
     
     
